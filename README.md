85c5420ffe315f74ccb96d283996b0e882d8ccfb
Rakíel <173973340+RakieI@users.noreply.github.com>
2025-12-23 00:06:41 -0300

    feat: add link/charpath, unlockanim, update for 1.0

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # UnlockCmd
 Unlock chars via commands on the select screen.
 
->Last tested on Ikemen GO v0.99 and Nightly Build (2025-01-12).  
+>Ikemen 1.0 Only.  
 >Module developed by Rakíel
 
 A module for Ikemen GO that enables configuring char unlock commands and customizing animations for hidden chars directly via def file.
 
 # Installation
 
-1. Extract archive content into "./external/mods" directory
+1. Extract archive content into "./external/mods/unlockCmd" directory
 2. Add your command and its settings to the unlockCmdConfig.def.
 3. Add sprites to unlockCmdSprites.sff, as required.
 5. Add sounds to unlockCmdSounds.snd, as required.
@@ -17,7 +17,9 @@ A module for Ikemen GO that enables configuring char unlock commands and customi
 # unlockCmdConfig.def parameters
 
 - name:
-  - The name of the char to be unlocked with the command. It must match exactly how it is written in select.def.  
+  - The name of the command to be used to unlock a char.
+- link or charpath:
+  - The char path, exactly the same as it’s defined in select.def.
 - command:
   - Write here the command to unlock the char (in the same format as commands are written in the char's CMD file).  
 - holdstart:
@@ -31,15 +33,19 @@ A module for Ikemen GO that enables configuring char unlock commands and customi
 - keep:
   - If true, will keep the char unlocked after closing the game, valid values are 1 (true) or 0 (false).
 - anim:
-  - The sprite or anim to be used as the char's portrait when locked. If omitted, the default random select icon from the portrait will be used, this uses the standard .air syntax, allowing you to specify a single sprite or define an anim.  
-   You must add your sprites to unlockCmdSprites.sff.  
+  - The sprite or anim to be used as the char's portrait when locked. If omitted, the default random select icon from the portrait will be used, this uses the standard .air syntax, allowing you to specify a single sprite or define an anim.
+  You must add your sprites to unlockCmdSprites.sff. 
+- unlockanim:
+  - The sprite or animation used as the char’s portrait when being unlocked. If omitted, it won’t be used. This follows the standard .air syntax, allowing you to specify either a single sprite or define an animation. If an animation is defined, the character will be unlocked at the end of the animation. Storyborads will be played after the unlock animation.
+  You must add your sprites to unlockCmdSprites.sff. 
 - storyboard:
   - Path to the storyboard to play when the char is unlocked
 
 # Example
   ```ini
 [UnlockConfig]
-name = SuaveDude
+name = SuaveUnlockCMD
+link = SuaveDude/suave.def
 command = ~F,F,F,B,B,B,s
 holdstart = 0
 unlocked = false
@@ -47,15 +53,17 @@ unlocksnd = 1,0,100
 hidden = 0
 keep = 0
 anim = 1,0, 0,0, -1
-storyboard = chars/suavedud/unlock.def
+unlockanim = 
+1,1, 0,0, 7
+1,2, 0,0, 7
+1,3, 0,0, 7
+1,4, 0,0, 7
+storyboard = chars/SuaveDude/unlock.def
  ```
 # Select.def
 
 After customizing the unlockCmdConfig, you need to edit the select.def as follows:
 
-``SuaveDude, hidden = 2, unlock = unlockCmd("SuaveDude")``
+``SuaveDude/suave.def, hidden = 2, unlock = unlockCmd("SuaveUnlockCDM")``
 
 You can also use hidden = 3 to turn the cell into a random select cell until the char is unlocked. Note that hidden = 1 will not work with the mod.
-
-After the command is executed, if you want an unlock animation, you need to add the line ``portrait.anim = *your anim*`` to the [Select Info] in the ``system.def``. By default, the animation number should be 9000, but if your char uses this animation for something else, 
-you should use a different number. If no corresponding animation exists, the char will default to using ``portrait.spr``. Make sure to use a very specific animation number here that doesn't exist in other chars to avoid issues.
--- unlockCmd.lua
+++ unlockCmd.lua
@@ -1,9 +1,9 @@
 -- UnlockCmd: Unlock chars via commands in the select screen.
--- Version: 1.1
--- Date: 01/14/2025
+-- Version: 1.2
+-- Date: 12/22/2025
 -- Author: Rakíel
--- Compatible with: Ikemen GO v0.99 and nightly
--- Description: This mod lets you create special commands to unlock chars in the select screen. These commands are defined in the unlockCmdConfig.json file.
+-- Compatible with: Ikemen GO 1.0
+-- Description: This mod lets you create special commands to unlock chars in the select screen. These commands are defined in the unlockCmdConfig.def file.
 -- To use this mod, you must declare specific parameters inside the unlockCmdConfig.def file. After that, you can call the commands in the select.def.
 
 --------------------------------------------------------
@@ -21,6 +21,7 @@ unlocksnd = 0,0,0
 hidden = 0
 keep = 0
 anim = 0,0, 0,0, -1
+link = 
 storyboard = 
 ]]
 
@@ -38,32 +39,56 @@ storyboard =
     local section = nil
     local isAnimSection = false
     local animLines = {}
+    local isUnlockAnimSection = false
+    local unlockAnimLines = {}
 
     local content = main.f_fileRead(path)
     content = content:gsub('([^\r\n;]*)%s*;[^\r\n]*', '%1')
     content = content:gsub('\n%s*\n', '\n')
     for line in content:gmatch('[^\r\n]+') do
         local lineCase = line:lower()
+
         if lineCase:match('^%s*%[unlockconfig%]%s*$') then
-            if section and isAnimSection then
-                section.anim = animLines
+            if section then
+                if #animLines > 0 then section.anim = animLines end
+                if #unlockAnimLines > 0 then section.unlockanim = unlockAnimLines end
             end
             section = {}
             table.insert(config.chars, section)
             isAnimSection = false
+            isUnlockAnimSection = false
             animLines = {}
+            unlockAnimLines = {}
         elseif section then
             local param, value = line:match('^%s*(.-)%s*=%s*(.-)%s*$')
             if param and value then
-                if param:match('anim') then
+
+                if isAnimSection and #animLines > 0 then
+                    section.anim = animLines
+                    animLines = {}
+                end
+
+                if isUnlockAnimSection and #unlockAnimLines > 0 then
+                    section.unlockanim = unlockAnimLines
+                    unlockAnimLines = {}
+                end
+
+                isAnimSection = false
+                isUnlockAnimSection = false
+                if param:match('^anim$') then
                     isAnimSection = true
                     table.insert(animLines, value)
-                elseif param:match('unlocksnd') then
+                elseif param:match('^unlockanim$') then
+                    isUnlockAnimSection = true
+                    table.insert(unlockAnimLines, value)
+                elseif param:match('^unlocksnd$') then
                     local values = {}
                     for num in value:gmatch('[^,]+') do
                         table.insert(values, tonumber(num) or num)
                     end
                     section[param] = values
+                elseif param:match('^link$') or param:match('^charpath$') then 
+                    section.link = value
                 elseif tonumber(value) then
                     section[param] = tonumber(value)
                 elseif value == "true" or value == "false" then
@@ -73,12 +98,10 @@ storyboard =
                 end
             elseif isAnimSection then
                 table.insert(animLines, line:match('^%s*(.-)%s*$'))
+            elseif isUnlockAnimSection then -- [NOVO]
+                table.insert(unlockAnimLines, line:match('^%s*(.-)%s*$'))
             end
         end
-    end
-
-    if section and isAnimSection then
-        section.anim = animLines
     end
 
     -- Default Values
@@ -105,6 +128,15 @@ storyboard =
             charData.storyboard = ""
         end
     end
+    
+    pathMap = {} -- Init
+    for _, charData in ipairs(config.chars) do
+        if charData.link and charData.link ~= "" then
+            -- Ex: pathMap["SF/EvilKen/EvilKen.def"] = "Super Command"
+            pathMap[charData.link] = charData.name
+        end
+    end
+    
     return config
 end
 
@@ -119,18 +151,49 @@ function unlockCmd(name)
     return false
 end
 
--- Table to store char anims
+-- Tables to store char anims
 local charAnims = {}
--- Function to create a custom anim
+local unlockAnims = {}
+local globalSff = sffNew('external/mods/unlockCmd/unlockCmdSprites.sff')
+
 function createAnim(charData)
-    if not charData.anim or #charData.anim == 0 then
-        return nil
+    if not charData.anim or #charData.anim == 0 or not globalSff then return nil end
+    local a = animNew(globalSff, table.concat(charData.anim, "\n"))
+
+    if a then
+        local params = motif.select_info.portrait
+        animSetLocalcoord(a, motif.info.localcoord[1], motif.info.localcoord[2])
+        animSetLayerno(a, 0)
+        local pScale = charData.portraitscale or 1.0
+        local referenceLocalcoord = 320
+        animSetScale(
+            a,
+            params.scale[1] * pScale * motif.info.localcoord[1] / referenceLocalcoord,
+            params.scale[2] * pScale * motif.info.localcoord[1] / referenceLocalcoord
+        )
+        animUpdate(a)
     end
-    local spriteData = sffNew('external/mods/unlockCmd/unlockCmdSprites.sff')  -- Loads the sff file
-    local animString = table.concat(charData.anim, "\n")
-    local anim = animNew(spriteData, animString)
-    spriteData = 0
-    return anim
+    return a
+end
+
+function createUnlockAnim(charData)
+    if not charData.unlockanim or #charData.unlockanim == 0 or not globalSff then return nil end
+    local a = animNew(globalSff, table.concat(charData.unlockanim, "\n"))
+    
+    if a then
+        local params = motif.select_info.portrait
+        animSetLocalcoord(a, motif.info.localcoord[1], motif.info.localcoord[2])
+        animSetLayerno(a, 0)
+        local pScale = charData.portraitscale or 1.0
+        local referenceLocalcoord = 320
+        animSetScale(
+            a,
+            params.scale[1] * pScale * motif.info.localcoord[1] / referenceLocalcoord,
+            params.scale[2] * pScale * motif.info.localcoord[1] / referenceLocalcoord
+        )
+        animUpdate(a)
+    end
+    return a
 end
 
 -- Function to play the unlock sound
@@ -169,6 +232,8 @@ function saveUnlockConfig(path, config)
                         table.insert(updatedContent, string.format("%s = %s", key, table.concat(value, ",")))
                     elseif key == "anim" and type(value) == "table" then
                         table.insert(updatedContent, string.format("%s = %s", key, table.concat(value, ",")))
+                    elseif key == "link" then
+                        table.insert(updatedContent, string.format("%s = %s", key, value))
                     elseif type(value) == "boolean" then
                         table.insert(updatedContent, string.format("%s = %s", key, value and "true" or "false"))
                     else
@@ -202,81 +267,82 @@ end
 --------------------------------------------------------
 --- Sprite/Anim rendering functions
 --------------------------------------------------------
+local function getCellFacing(default, col, row)
+	local cell = motif.select_info.cell[col .. '-' .. row]
+	if cell ~= nil and cell.facing ~= 0 then
+		return cell.facing
+	end
+	return default
+end
+
 function drawLockedCell()
-    -- Draw cell art
     for row = 1, motif.select_info.rows do
         for col = 1, motif.select_info.columns do
             local t = start.t_grid[row][col]
             if t.skip ~= 1 then
-                --draw face cell
-                if t.char ~= nil and t.hidden == 0 then
-                    main.f_animPosDraw(
-                        start.f_getCharData(t.char_ref).cell_data,
-                        motif.select_info.pos[1] + t.x + motif.select_info.portrait_offset[1],
-                        motif.select_info.pos[2] + t.y + motif.select_info.portrait_offset[2],
-                        (motif.select_info['cell_' .. col .. '_' .. row .. '_facing'] or motif.select_info.portrait_facing)
-                    )
-                end
-                if t.hidden == 2 then
-                    local charAnim = charAnims[t.char]  -- Use specific char anim
-                    for _, charData in ipairs(unlockConfig.chars) do
-                        if charData.name == t.char and charData.hidden then
-                            hidden = (charData.hidden == 1)
-                            break
-                        end
-                    end
-                    if charAnim and not hidden then
-                        if motif.select_info.showemptyboxes == 0 then
-                            main.f_animPosDraw(
-                                motif.select_info.cell_bg_data,
-                                motif.select_info.pos[1] + t.x,
-                                motif.select_info.pos[2] + t.y,
-                                (motif.select_info['cell_' .. col .. '_' .. row .. '_facing'] or motif.select_info.cell_bg_facing)
-                            )
-                        end
-                        main.f_animPosDraw(
-                            charAnim,
-                            motif.select_info.pos[1] + t.x + motif.select_info.portrait_offset[1],
-                            motif.select_info.pos[2] + t.y + motif.select_info.portrait_offset[2],
-                            (motif.select_info['cell_' .. col .. '_' .. row .. '_facing'] or motif.select_info.cell_random_facing)
-                        )
-                        animSetScale(
-                            charAnim,
-                            motif.select_info.portrait_scale[1] / (motifViewport43(2) / motifLocalcoord(0)),
-                            motif.select_info.portrait_scale[2] / (motifViewport43(2) / motifLocalcoord(0)),
-                            false
-                        )
-                    elseif not hidden then -- fallback to the screenpack default '?'
-                        main.f_animPosDraw(
-                            motif.select_info.cell_random_data,
-                            motif.select_info.pos[1] + t.x + motif.select_info.portrait_offset[1],
-                            motif.select_info.pos[2] + t.y + motif.select_info.portrait_offset[2],
-                            (motif.select_info['cell_' .. col .. '_' .. row .. '_facing'] or motif.select_info.cell_random_facing)
-                        )
+                
+                -- Locate the mod data for this cell
+                local configName = pathMap[t.char] or t.char
+                local targetCharData = nil
+                for _, cd in ipairs(unlockConfig.chars) do
+                    if cd.name == configName then 
+                        targetCharData = cd 
+                        break 
                     end
                 end
-            end
-        end
-    end
-    --draw done cursors
-    for side = 1, 2 do
-        for _, v in pairs(start.p[side].t_selected) do
-            if v.cursor ~= nil then
-                --get cell coordinates
-                local x = v.cursor[1]
-                local y = v.cursor[2]
-                local t = start.t_grid[y + 1][x + 1]
-                --render only if cell is not hidden
-                if t.hidden ~= 1 and t.hidden ~= 2 then
-                    start.f_drawCursor(v.pn, x, y, '_cursor_done', true)
-                end
-            end
-        end
-       if not start.p[side].selEnd then
-            --for each player with active controls
-            for k, v in ipairs(start.p[side].t_selCmd) do
-                if v.selectState < 4 and start.f_selGrid(start.c[v.player].cell + 1).hidden ~= 1 and not start.c[v.player].blink then
-                    start.f_drawCursor(v.player, start.c[v.player].selX, start.c[v.player].selY, '_cursor_active', false)
+
+                if targetCharData then
+
+                    local isBuffer = (targetCharData.unlockTimer or 0) < 0
+                    
+                    if t.hidden == 2 or isBuffer then
+                        local isHidden = (targetCharData.hidden == 1)
+                        local pX = motif.select_info.pos[1] + t.x + motif.select_info.portrait.offset[1]
+                        local pY = motif.select_info.pos[2] + t.y + motif.select_info.portrait.offset[2]
+                        local pFacing = getCellFacing(motif.select_info.portrait.facing, col - 1, row - 1)
+
+                        if targetCharData.unlockTimer and targetCharData.unlockTimer ~= 0 then
+                            local uAnim = unlockAnims[configName]
+                            local charAnim = charAnims[configName]
+
+                            if targetCharData.unlockTimer > 0 and uAnim then
+                                main.f_animPosDraw(uAnim, pX, pY, pFacing)
+                            else
+                                if charAnim then
+                                    main.f_animPosDraw(charAnim, pX, pY, pFacing)
+                                else
+                                    main.f_animPosDraw(motif.select_info.cell.random.AnimData, pX, pY, pFacing)
+                                end
+                                if uAnim then
+                                    main.f_animPosDraw(uAnim, pX, pY, pFacing)
+                                end
+                            end
+                        -- Static frame
+                        elseif not targetCharData.unlocked then
+                            if not isHidden then
+                                -- Cell BG
+                                if motif.select_info.showemptyboxes then
+                                    main.f_animPosDraw(
+                                        motif.select_info.cell.bg.AnimData,
+                                        motif.select_info.pos[1] + t.x,
+                                        motif.select_info.pos[2] + t.y,
+                                        getCellFacing(motif.select_info.cell.bg.facing, col - 1, row - 1)
+                                    )
+                                end
+                                -- Custom Sprite, Anim or default '?'
+                                local charAnim = charAnims[configName]
+                                if charAnim then
+                                    main.f_animPosDraw(charAnim, pX, pY, pFacing)
+                                else
+                                    main.f_animPosDraw(
+                                        motif.select_info.cell.random.AnimData,
+                                        pX, pY,
+                                        getCellFacing(motif.select_info.cell.random.facing, col - 1, row - 1)
+                                    )
+                                end
+                            end
+                        end
+                    end
                 end
             end
         end
@@ -287,34 +353,88 @@ end
 --- Hooks and command check code
 --------------------------------------------------------
 function checkcommand()
-    for p = 1, #main.t_players do
-        for _, charData in ipairs(unlockConfig.chars) do
-            if not charData.unlocked then
-                main.f_commandAdd("hold_start", "/s", 1, 1)
-                main.f_commandAdd(charData.name, charData.command, 150, 1)
-                local commandExecuted = commandGetState(main.t_cmd[p], charData.name)
-                if charData.holdstart == 1 then
-                    commandExecuted = commandExecuted and commandGetState(main.t_cmd[p], "hold_start")
-                end
-                if commandExecuted then
+    -- Timer
+    for _, charData in ipairs(unlockConfig.chars) do
+        
+        if not charAnims[charData.name] and charData.anim then
+            charAnims[charData.name] = createAnim(charData)
+        end
+        if not unlockAnims[charData.name] and charData.unlockanim then
+            unlockAnims[charData.name] = createUnlockAnim(charData)
+        end
+        if charData.unlockTimer then
+            if charData.unlockTimer > 0 then
+                charData.unlockTimer = charData.unlockTimer - 1
+                start.needUpdateDrawList = true
+                if charData.unlockTimer == 0 then
                     charData.unlocked = true
+                    -- Buffer for Ikemen load default portraits
                     main.f_unlock(true)
-                    playUnlockSound(charData)
-                    start.needUpdateDrawList = true
+                    charData.unlockTimer = -3
+                    -- Unlock storyboard
                     if charData.storyboard ~= nil and charData.storyboard ~= "" then
-                    local path = charData.storyboard
-                        launchStoryboard(path)
+                        launchStoryboard(charData.storyboard)
                     end
                     if charData.keep == 1 then
-                        -- Save the .def
                         saveUnlockConfig('external/mods/unlockCmd/unlockCmdConfig.def', unlockConfig)
                     end
                 end
+                -- Buffer
+                elseif charData.unlockTimer < 0 then
+                    charData.unlockTimer = charData.unlockTimer + 1
+                -- Timer Reset
+                if charData.unlockTimer == 0 then
+                    charData.unlockTimer = nil
+                end
             end
-            -- Load sprites/anim for locked chars
-            if not charAnims[charData.name] and charData.anim then
-                charAnims[charData.name] = createAnim(charData)
+        end
+    end
+
+    -- Check Player Inputs
+    for p = 1, #main.t_players do
+        for _, charData in ipairs(unlockConfig.chars) do
+            
+            if charData.unlocked or (charData.unlockTimer and charData.unlockTimer > 0) then
+                goto continue
             end
+            main.f_commandAdd("hold_start", "/s", 1, 1)
+            main.f_commandAdd(charData.name, charData.command, 150, 1)
+            
+            local commandExecuted = commandGetState(main.t_cmd[p], charData.name)
+            if charData.holdstart == 1 then
+                commandExecuted = commandExecuted and commandGetState(main.t_cmd[p], "hold_start")
+            end
+
+            if commandExecuted then
+                local uAnim = unlockAnims[charData.name]
+                local duration = 0
+                
+                -- Check unlockanim and gets its duration
+                if uAnim then
+                    animReset(uAnim)
+                    animUpdate(uAnim)
+                    duration = select(1, animGetLength(uAnim))
+                    duration = math.floor(duration / 1.1)
+                end
+
+                if duration > 0 then
+                    charData.unlockTimer = duration
+                    playUnlockSound(charData)
+                else
+                    charData.unlocked = true
+                    main.f_unlock(true)
+                    playUnlockSound(charData)
+                    charData.unlockTimer = -3
+                    if charData.storyboard ~= nil and charData.storyboard ~= "" then
+                        launchStoryboard(charData.storyboard)
+                    end
+                    if charData.keep == 1 then 
+                        saveUnlockConfig('external/mods/unlockCmd/unlockCmdConfig.def', unlockConfig) 
+                    end
+                end
+                start.needUpdateDrawList = true
+            end
+            ::continue::
         end
     end
     drawLockedCell()
